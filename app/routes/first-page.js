169c7a7ab8412bb0b96e9dc51992dd3701d29a95
LiShuxue <1149926505@qq.com>
2017-11-03 13:48:09 +0800

    路由生命周期和model对象

---
+++ app/routes/first-page.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+import RSVP from 'rsvp';
+
+export default Ember.Route.extend({
+    beforeModel(transition) {
+        // beforeModel() receives the current transition as an argument
+        console.log('before model...');
+        console.log(transition);
+    },
+    model() {
+        console.log('model...')
+        let test1 = [{
+            title: 'first title',
+            body: 'first body'
+        }, {
+            title: 'second title',
+            body: 'second body'
+        }, {
+            title: 'third title',
+            body: 'third body'
+        }];
+        let test2 = [{
+            title: 'first title2',
+            body: 'first body2'
+        }, {
+            title: 'second title2',
+            body: 'second body2'
+        }, {
+            title: 'third title2',
+            body: 'third body2'
+        }];
+
+        return RSVP.hash({
+            test1,
+            test2
+        });
+    },
+    afterModel(model, transition) {
+        //It receives the resolved model as the first parameter and the transition as the second one.
+        console.log('after model...');
+        console.log(model);
+        console.log(transition);
+    },
+    redirect(model, transition) {
+        console.log('redirect...');
+    }
+});
