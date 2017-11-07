9af5b319efde2e3dd7e6aec5ee9266bad871f879
LiShuxue <1149926505@qq.com>
2017-11-07 10:42:53 +0800

    使用ember-ajax发送ajax请求来获得数据

--- app/routes/third-page.js
+++ app/routes/third-page.js
@@ -1,13 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    ajax: Ember.inject.service(),
     model() {
         return this.store.findAll('user');
     },
     actions: {
         getUser() {
             Ember.Logger.log('get user..');
-            this.store.findAll('user');
+            const ajax = this.get('ajax');
+            ajax.request('/users').then( response => {
+                Ember.Logger.log(response);
+            }).catch( error => {
+                Ember.Logger.log(error);
+            });
+            //You can skip setting the method or type keys in your options object when calling request(url, options) by instead calling post(url, options), put(url, options), patch(url, options) or del(url, options).
+            // this.get('ajax').post('/XXX', {
+            //     data: {
+            //         foo: 'bar'
+            //     }
+            // });
         }
     }
 });
